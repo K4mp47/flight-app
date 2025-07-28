6ba97a258675a72e0a262eaf86fbb6ff2d6194dd
STMarcoStevanato <131982944+STMarcoStevanato@users.noreply.github.com>
2025-07-28 02:27:28 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ from api.routes import register_routes
 from sqlalchemy.orm import sessionmaker
 from api.models import *
 from flask_jwt_extended import JWTManager
+from api.utils.blacklist import blacklisted_tokens
 
 
 def create_app():
@@ -13,6 +14,11 @@ def create_app():
     CORS(app, origins="http://localhost:3000")
     register_routes(app)
     jwt = JWTManager(app)
+
+    def check_if_token_revoked(jwt_header, jwt_payload):
+        jti = jwt_payload["jti"]
+        return jti in blacklisted_tokens
+
     return app
 
 if __name__ == "__main__":

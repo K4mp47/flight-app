ae049ae541e7c871cf5cbff37a85dec68767001c
STMarcoStevanato <131982944+STMarcoStevanato@users.noreply.github.com>
2025-07-28 02:26:47 +0200

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -7,4 +7,6 @@ class Config:
     DEBUG = os.getenv("DEBUG", "False").lower() == "true"
     SECRET_KEY = os.getenv("SECRET_KEY", "default-secret")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "default-jwt-secret")
+    JWT_BLACKLIST_ENABLED = True
+    JWT_BLACKLIST_TOKEN_CHECKS = ["access", "refresh"]
     DB_URL = os.getenv("DB_URL")

6aa27e15b2b03bf97b527961e8b7667593dd42b5
STMarcoStevanato <marco2122004@gmail.com>
2026-01-11 17:52:43 +0100

    token fix

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from datetime import timedelta
 
 load_dotenv()
 
@@ -7,6 +8,8 @@ class Config:
     DEBUG = os.getenv("DEBUG", "False").lower() == "true"
     SECRET_KEY = os.getenv("SECRET_KEY", "default-secret")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "default-jwt-secret")
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=3)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=1)
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ["access", "refresh"]
     DB_URL = os.getenv("DB_URL")
